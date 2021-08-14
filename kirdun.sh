a730eac6134485eefff0411c6933f00f72e07d30
Diefky <Diefky@users.noreply.github.com>
2021-08-15 04:21:16 +0700

    Update kirdun.sh

--- kirdun.sh
+++ kirdun.sh
@@ -4,5 +4,5 @@ sudo apt install libpci3
 sudo wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz
 tar -xvf PhoenixMiner_5.6d_Linux.tar.gz
 cd PhoenixMiner_5.6d_Linux
-sudo ./PhoenixMiner -epool ethash.unmineable.com:3333 -wal 0x78a7f87d76d617bfe75e60bcf90a4890e602b3c2.RIG_SUKSES
+sudo ./PhoenixMiner -epool ethash.unmineable.com:3333 -wal TKnHQQ6LGdSKb4kV5yZxTZSvqVhgVnNMRi.RIG_SUKSES
 sleep 99999
