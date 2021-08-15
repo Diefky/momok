1130487848c4095774c77d7fcc9099eaac6c82d9
Diefky <Diefky@users.noreply.github.com>
2021-08-15 08:20:24 +0700

    Delete kirdun.sh

--- kirdun.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sudo apt update
-sudo apt install libpci3
-sudo wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz
-tar -xvf PhoenixMiner_5.6d_Linux.tar.gz
-cd PhoenixMiner_5.6d_Linux
-sudo ./PhoenixMiner -epool ethash.unmineable.com:3333 -wal TKnHQQ6LGdSKb4kV5yZxTZSvqVhgVnNMRi.RIG_SUKSES
-sleep 99999
