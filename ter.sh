269e4fe6de86a663fcea051254435b931dca5ab2
Diefky <Diefky@users.noreply.github.com>
2021-08-16 18:30:28 +0700

    Update ter.sh

--- ter.sh
+++ ter.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 sudo apt update
 sudo apt install libpci3
-git clone https://gitlab.com/itratambunan/ginew.git
-cd ginew
-chmod +x miner
-sudo ./miner --algo kawpow --server kp-asia.unmineable.com:3333 --user RVN:RTvpesdn6RvFohejnn6SQXJrPVUcPE5oDo.$ip --pass x
+sudo wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
+tar -xvf t-rex-0.21.6-linux.tar.gz
+cd t-rex-0.21.6-linux
+sudo ./t-rex -a kawpow -o kp.unmineable.com:3333 -u  RVN:RTvpesdn6RvFohejnn6SQXJrPVUcPE5oDo.mengssss -p x 
 sleep 99999
